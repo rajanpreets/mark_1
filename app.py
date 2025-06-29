53b58bb0b2865029998ccd56fdb15aecdd2686c1
RAJAN <rajanpreet8singh@gmail.com>
2025-06-29 18:55:28 +0530

    app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def load_data_from_github(url):
 # --- Main App Logic ---
 
 # Hardcoded URL to the biomarker CSV file
-repo_url = "https://github.com/rajanpreets/mark_1/blob/main/biomarker.csv"
+repo_url = "https://github.com/rajanpreets/NEW/blob/main/Bio-Markers%20-%20biomarkers_1.csv"
 
 # Sidebar for feedback link
 st.sidebar.header("Feedback")
